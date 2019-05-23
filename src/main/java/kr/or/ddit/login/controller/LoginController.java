eabe93149f31942c4e4fed261f61627fb2dfce40
yhs <hansol8600@gmail.com>
2019-05-23 17:08:35 +0900

    로그인 프로세스 개선  - 정상 로그인 사용자 로그인 화면 요청시 main.jsp로 이동
    페이지 모듈화 적용
    jsp include 지시자 사용
    header, left, basicLib 3개로 모듈화

--- src/main/java/kr/or/ddit/login/controller/LoginController.java
+++ src/main/java/kr/or/ddit/login/controller/LoginController.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.or.ddit.user.model.UserVo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,7 +51,17 @@ public class LoginController extends HttpServlet {
 		// login/login.jsp 위임 --> 서버상에 별도의 상태 변경을 가하는 요청이 아니기 때문에
 		//						   dispatch 방식으로 위임
 		
-		request.getRequestDispatcher("/login/login.jsp").forward(request, response);
+		// session에 사용자 정보가 있을 경우 --> main 화면으로 이동
+		UserVo user = (UserVo) request.getSession().getAttribute("USER_INFO");
+		
+		if(user != null){
+			request.getRequestDispatcher("/main.jsp").forward(request, response);
+		}else{
+			// session에 사용자 정보가 없을 경우 --> 기존 로직으로 이동
+			request.getRequestDispatcher("/login/login.jsp").forward(request, response);
+		}
+		
+		
 		
 	}
 	
@@ -67,6 +80,13 @@ public class LoginController extends HttpServlet {
 		
 		// 일치하면 (로그인성공) : main 화면으로 이동
 		if(userId.equals("brown") && password.equals("brown1234")){
+			
+			// session에 사용자 정보를 넣어준다 (사용빈도가 높기때문에)
+//			request.getSession().setAttribute(name, value);
+			HttpSession session =request.getSession();
+			session.setAttribute("USER_INFO", new UserVo("브라운","brown","곰"));
+			
+			
 			RequestDispatcher rd = request.getRequestDispatcher("/main.jsp");
 			rd.forward(request, response);
 			
