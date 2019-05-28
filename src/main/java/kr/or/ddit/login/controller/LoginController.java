796fdc03b47156786a1d7fba100c22a2fda45e22
yhs <hansol8600@gmail.com>
2019-05-28 16:36:58 +0900

    serverside cookie 설정 쿠키조회 : Cookie[] cookies = requeset.getCookies(); 쿠키설정 : Cookie cookie = new Cookie("쿠키이름","쿠키값"); 		 cookie.setMaxAge(초단위); 		 response.addCookie(cookie);

--- src/main/java/kr/or/ddit/login/controller/LoginController.java
+++ src/main/java/kr/or/ddit/login/controller/LoginController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -46,6 +47,10 @@ public class LoginController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		logger.debug("LoginController doGet()");
 		
+		for(Cookie cookie : request.getCookies()){
+			logger.debug("cookie : {}, {}", cookie.getName(),cookie.getValue());
+		}
+		
 		// login 화면을 처리해줄 누군가??에게 위임
 		// 단순 login화면을 html로 응답을 생성해주는 작업이 필요
 		// login/login.jsp 위임 --> 서버상에 별도의 상태 변경을 가하는 요청이 아니기 때문에
@@ -61,12 +66,11 @@ public class LoginController extends HttpServlet {
 			request.getRequestDispatcher("/login/login.jsp").forward(request, response);
 		}
 		
-		
-		
 	}
 	
 	// 로그인 요청을 처리
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logger.debug("parameter rememberme  : {}", request.getParameter("rememberme"));
 		logger.debug("parameter userId : {}",request.getParameter("userId") );
 		logger.debug("parameter password : {}",request.getParameter("password") );
 		
@@ -78,8 +82,27 @@ public class LoginController extends HttpServlet {
 		// 해당 사용자 정보를 이용하여 사용자가 보낸 userId, password가 일치하는지 검사
 		// --> userId : brown 이고 password : brown1234라는 값일 때 통과, 그 이외 값은 불일치
 		
+	
+		
 		// 일치하면 (로그인성공) : main 화면으로 이동
 		if(userId.equals("brown") && password.equals("brown1234")){
+			
+			// remember 파라미터가 존재할 경우 userId, rememberme cookie 설정해준다.
+			// remember 파라미터가 존재하지 않을 경우 userId, rememberme cookie 삭제한다.
+			int cookieMaxAge=0;
+			if(request.getParameter("rememberme")!=null){
+				cookieMaxAge =60*60*24*30;
+			}	
+			Cookie userIdCookie = new Cookie("userId",userId);
+			userIdCookie.setMaxAge(cookieMaxAge);
+			
+			Cookie rememberMeCookie = new Cookie("rememberme","true");
+			rememberMeCookie.setMaxAge(cookieMaxAge);
+			
+			response.addCookie(userIdCookie);
+			response.addCookie(rememberMeCookie);
+			
+			
 			
 			// session에 사용자 정보를 넣어준다 (사용빈도가 높기때문에)
 //			request.getSession().setAttribute(name, value);
